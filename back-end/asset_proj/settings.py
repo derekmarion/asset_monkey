cfc1ec9b27fe3a8fec6e3e76dd12e9af2ce71308
Derek Marion <derek.j.marion@gmail.com>
2023-12-13 15:28:48 -0800

    Fix secret key import

--- back-end/asset_proj/settings.py
+++ back-end/asset_proj/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 env = dotenv_values("../.env")
+SECRET_KEY = env.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
