f52d31b5efbb89bfff3ff8e66728da4101daa6ba
Derek Marion <derek.j.marion@gmail.com>
2023-12-15 11:28:41 -0800

    Remove Bootstrap, install Tailwind

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import router from "./router";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
