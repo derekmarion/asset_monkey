53845b980051a5a498e2f3f294f295ceb88305db
Derek Marion <derek.j.marion@gmail.com>
2023-12-15 12:33:45 -0800

    Apply basic styling to item pages

--- front-end/src/pages/InventoryPage.jsx
+++ front-end/src/pages/InventoryPage.jsx
@@ -25,21 +25,27 @@ export const Inventory = () => {
 
   const handleButtonClick = (id) => {
     navigate(`/inventory/${id}/`);
-}
+  };
 
   return (
-    <>
-    {items.map((item) => (
-      <div key={item.id}>
-        {/* Render each item's details */}
-        <h3>Name: {item.name}</h3>
-        <p>Category: {item.category}</p>
-        <p>Quantity: {item.quantity}</p>
-        <p>Price: ${item.price}</p>
-        <button onClick={()=>handleButtonClick(item.id)}>More Details</button>
-        {/* Add more details as needed */}
-      </div>
-    ))}
-    </>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {/* Render each item as a card */}
+      {items.map((item) => (
+        <div key={item.id} className="bg-white rounded-lg shadow-md p-4">
+          {/* Render each item's details */}
+          <h3 className="text-lg font-semibold mb-2">{item.name}</h3>
+          <p className="text-gray-600 mb-2">Category: {item.category}</p>
+          <p className="text-gray-600 mb-2">Quantity: {item.quantity}</p>
+          <p className="text-gray-600 mb-2">Price: ${item.price}</p>
+          <button
+            onClick={() => handleButtonClick(item.id)}
+            className="bg-slate-900 text-white px-4 py-2 rounded-full hover:bg-slate-700"
+          >
+            More Details
+          </button>
+          {/* Add more details as needed */}
+        </div>
+      ))}
+    </div>
   );
 };
--- front-end/src/pages/SingleItemPage.jsx
+++ front-end/src/pages/SingleItemPage.jsx
@@ -12,7 +12,7 @@ export const SingleItem = () => {
   const [category, setCategory] = useState("");
   const [name, setName] = useState("");
   const [quantity, setQuantity] = useState(0);
-  const [price, setPrice] = useState(1.00);
+  const [price, setPrice] = useState(1.0);
   const [serial_num, setSerial_Num] = useState("SN12345");
   const [file, setFile] = useState(null);
 
@@ -37,23 +37,26 @@ export const SingleItem = () => {
   const updateItem = async (e) => {
     e.preventDefault();
     let data = {
-        "name": name,
-        "category": category,
-        "quantity": quantity,
-        "price": price,
-        "serial_num": serial_num,
+      name: name,
+      category: category,
+      quantity: quantity,
+      price: price,
+      serial_num: serial_num,
     };
     try {
-        const response = await api.put(`/inventories/all_items/${id}/`, data);
-        if (file !== null) {
-          const formData = new FormData();
-          formData.append('file', file);
-          const fileUploadResponse = await api.post(`/files/upload/${id}/`, formData);
-        }
-        setItem(response.data);
-        setEditing(false);
-        } catch (error){
-    alert("Something went wrong");
+      const response = await api.put(`/inventories/all_items/${id}/`, data);
+      if (file !== null) {
+        const formData = new FormData();
+        formData.append("file", file);
+        const fileUploadResponse = await api.post(
+          `/files/upload/${id}/`,
+          formData
+        );
+      }
+      setItem(response.data);
+      setEditing(false);
+    } catch (error) {
+      alert("Something went wrong");
     }
   };
 
@@ -63,33 +66,86 @@ export const SingleItem = () => {
 
   const uploadFile = (e) => {
     setFile(e.target.files[0]);
-  }
+  };
 
   return (
-    editing ? (
-      <div key={item.id}>
-        {/*Editing item*/}
-        <form onSubmit={(e) => updateItem(e)}>
-        <h3>Name: <input type="text" placeholder={item.name} onChange={(e) => setName(e.target.value)} /></h3>
-        <p>Category: <input type="text" placeholder={item.category} onChange={(e) => setCategory(e.target.value)} /></p>
-        <p>Quantity: <input type="text" placeholder={item.quantity} onChange={(e) => setQuantity(e.target.value)} /></p>
-        <p>Price: <input type="text" placeholder={item.price} onChange={(e) => setPrice(e.target.value)} /></p>
-        <p>Serial Number: <input type="text" placeholder={item.serial_num} onChange={(e) => setSerial_Num(e.target.value)} /></p>
-        <input type="file" onChange={(e) => uploadFile(e)} />
-        <input type="submit" value="Save" />
-        <button onClick={()=>setEditing(false)}>Cancel</button>
-        </form>
+    <div className="flex justify-center items-center">
+      <div className="bg-white rounded-lg shadow-md p-4 mt-20">
+        {editing ? (
+          // Editing item
+          <form onSubmit={(e) => updateItem(e)}>
+            <h3 className="text-lg font-semibold mb-2">
+              Name:{" "}
+              <input
+                type="text"
+                placeholder={item.name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </h3>
+            <p className="text-gray-600 mb-2">
+              Category:{" "}
+              <input
+                type="text"
+                placeholder={item.category}
+                onChange={(e) => setCategory(e.target.value)}
+              />
+            </p>
+            <p className="text-gray-600 mb-2">
+              Quantity:{" "}
+              <input
+                type="text"
+                placeholder={item.quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+              />
+            </p>
+            <p className="text-gray-600 mb-2">
+              Price:{" "}
+              <input
+                type="text"
+                placeholder={item.price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
+            </p>
+            <p className="text-gray-600 mb-2">
+              Serial Number:{" "}
+              <input
+                type="text"
+                placeholder={item.serial_num}
+                onChange={(e) => setSerial_Num(e.target.value)}
+              />
+            </p>
+            <input type="file" onChange={(e) => uploadFile(e)} />
+            <input
+              className="bg-slate-900 text-white px-4 py-2 rounded-full hover:bg-slate-700"
+              type="submit"
+              value="Save"
+            />
+            <button
+              className="bg-slate-900 text-white px-4 py-2 rounded-full hover:bg-slate-700"
+              onClick={() => setEditing(false)}
+            >
+              Cancel
+            </button>
+          </form>
+        ) : (
+          // Viewing item
+          <>
+            <h3 className="text-lg font-semibold mb-2">Name: {item.name}</h3>
+            <p className="text-gray-600 mb-2">Category: {item.category}</p>
+            <p className="text-gray-600 mb-2">Quantity: {item.quantity}</p>
+            <p className="text-gray-600 mb-2">Price: ${item.price}</p>
+            <p className="text-gray-600 mb-2">
+              Serial Number: {item.serial_num}
+            </p>
+            <button
+              className="bg-slate-900 text-white px-4 py-2 rounded-full hover:bg-slate-700"
+              onClick={() => setEditing(true)}
+            >
+              Edit
+            </button>
+          </>
+        )}
       </div>
-    ) : (
-      <div key={item.id}>
-         {/*Viewing item*/}
-        <h3>Name: {item.name}</h3>
-        <p>Category: {item.category}</p>
-        <p>Quantity: {item.quantity}</p>
-        <p>Price: ${item.price}</p>
-        <p>Serial Number: {item.serial_num}</p>
-        <button onClick={()=>setEditing(true)}>Edit</button>
-      </div>
-    )
+    </div>
   );
 };
