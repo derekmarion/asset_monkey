a396cd05d870013a386c36b3b97948fc72d39cd7
Derek Marion <derek.j.marion@gmail.com>
2023-12-19 15:24:03 -0800

    Improve error logging for api calls

--- front-end/src/components/Register.jsx
+++ front-end/src/components/Register.jsx
@@ -13,17 +13,23 @@ export const SignUp = () => {
 
     const signUp = async (e) => {
         e.preventDefault();
-        let response = await api.post("users/signup", {
+        let responseUser = await api.post("users/signup", {
             email: userName,
             password: password,
         });
-        let user = response.data.user;
-        let token = response.data.token;
+        let user = responseUser.data.user;
+        let token = responseUser.data.token;
+        console.log(responseUser)
         // Store the token securely (e.g., in localStorage or HttpOnly cookies)
         localStorage.setItem("token", token);
         api.defaults.headers.common["Authorization"] = `Token ${token}`;
         // set the user using with useContext to allow all other pages that need user information
         setUser(user);
+        // create an inventory for the user
+        let responseInventory = await api.post("inventories/inventory_manager/", {
+          user: userName
+        });
+        console.log(responseInventory)
         // navigate("/home"); should probably set this to the user's inventory page
     };
 
