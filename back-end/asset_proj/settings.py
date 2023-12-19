4c9e2e34eae32aeb6b506f07424f346fbfc08aa8
Derek Marion <derek.j.marion@gmail.com>
2023-12-19 15:08:27 -0800

    Add Dockerfiles so application is fully containerized

--- back-end/asset_proj/settings.py
+++ back-end/asset_proj/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-env = dotenv_values(".env")
+env = dotenv_values(".env/.django.env")
 SECRET_KEY = env.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
