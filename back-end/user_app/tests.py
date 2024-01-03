e9798074b3202f540f83fd7d47ea7084bc83fe41
Derek Marion <derek.j.marion@gmail.com>
2024-01-03 08:46:26 -0800

    Add test for signup view

--- back-end/user_app/tests.py
+++ back-end/user_app/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.urls import reverse
+
 
 # Create your tests here.
+class SignUpViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse("signup")
+
+    def test_signup(self):
+        # Hit signup endpoint
+        response = self.client.post(
+            self.url,
+            format="json",
+            data={"email": "test@example.com", "password": "testpassword"},
+        )
+        # Check if the login was successful
+        self.assertEqual(response.status_code, 201)
+
+        # Retrieve the token from the response
+        token_key = response.data.get("token", None)
+        self.assertIsNotNone(token_key, "Token not found in response")
+
+        # Include the Authorization token in the request headers
+        self.client.credentials(AUTHORIZATION=f"Token {token_key}")
