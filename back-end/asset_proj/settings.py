7d1db8c409d4415dddcf3845e4c4db61ce6c6a79
Derek Marion <derek.j.marion@gmail.com>
2024-01-03 11:50:58 -0800

    Add conditional workflow settings and db health check

--- back-end/asset_proj/settings.py
+++ back-end/asset_proj/settings.py
@@ -96,10 +96,23 @@ DATABASES = {
         "NAME": os.environ.get('POSTGRES_NAME'),
         "USER": os.environ.get('POSTGRES_USER'),
         "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
-        "HOST": 'localhost',
+        "HOST": 'db',
         "PORT": 5432,
     }
 }
+
+# CI Github Workflow Settings
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 
 # Password validation
